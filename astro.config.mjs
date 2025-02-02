0cff5ffd6f18b2d34704139f421ab0cca471ddff
histcat <jessejessejeson@163.com>
2025-02-02 21:10:37 +0800

    fix:side bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    format: "file",
-  },
   integrations: [vue(), icon(), pagefind()],
   markdown: {
     remarkPlugins: ["remark-math"],
